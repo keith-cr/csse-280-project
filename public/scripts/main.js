781a54a6c0138ee85a4f50a559bc91da3809de96
Gnarwhal <gnarlynarwhal8@gmail.com>
2020-10-25 19:19:04 -0500

    Added login page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,36 +1,79 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
-
-/** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
+rhit.authManager = null;
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
-
-rhit.ClassName = class {
+rhit.LoginPageController = class {
 	constructor() {
+		document.querySelector("#loginButton").addEventListener("click", (event) => {
+			rhit.authManager.signIn();
+		});
+	}
+}
 
+rhit.AuthManager = class {
+	constructor() {
+		this._user = null;
 	}
 
-	methodName() {
+	beginListening(changeListener) {
+		firebase.auth().onAuthStateChanged((user) => {
+			this._user = user;
+			changeListener();
+		});
+	}
 
+	signIn() {
+		Rosefire.signIn("f1b9e751-e617-465c-9ebe-074d22062af3", (err, rfUser) => {
+			if (err) {
+				console.log("Rosefire error!");
+			}
+			console.log("Rosefire success!", rfUser);
+			firebase.auth().signInWithCustomToken(rfUser.token).catch((error) => {
+				if (error.code === "auth/invalid-custom-token") {
+					console.error("Invalid token");
+				} else {
+					console.error("Custom auth error", error.code, error.message);
+				}
+			});
+		});
+	}
+
+	signOut() {
+		firebase.auth().signOut().catch(() => {
+			console.log("Signed out");
+		});
+	}
+
+	get isAuthenticated() {
+		return !!this._user;
+	}
+
+	get uid() {
+		return this._uid;
+	}
+}
+
+rhit.checkForRedirects = () => {
+	if (document.querySelector("#loginPage") && rhit.authManager.isAuthenticated) {
+		window.location.href = "/now.html";
+	} else if (!document.querySelector("#loginPage") && !rhit.authManager.isAuthenticated) {
+		window.location.href = "/";
+	}
+}
+
+rhit.initializePage = () => {
+	if (document.querySelector("#loginPage")) {
+		new rhit.LoginPageController();
 	}
 }
 
 /* Main */
-/** function and class syntax examples */
 rhit.main = function () {
-	console.log("Ready");
+	rhit.authManager = new rhit.AuthManager();
+	rhit.authManager.beginListening(() => {
+		rhit.checkForRedirects();
+		rhit.initializePage();
+	});
 };
 
 rhit.main();
